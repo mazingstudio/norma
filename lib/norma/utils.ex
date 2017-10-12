27d6e3c9ca104b9061c59bf4223d816da4cc4c7c
Zura Guerra <guerrazuramx@gmail.com>
2017-10-12 18:48:21 -0500

    Fix bug where fragments and queries didn't merge with their prefix symbol

--- lib/norma/utils.ex
+++ lib/norma/utils.ex
@@ -28,15 +28,21 @@ defmodule Norma.Utils do
                      scheme: scheme,
                      query: query,
                      fragment: fragment}) do
-    form_scheme(scheme)      # "http://"
-    |> safe_concat(host)     # "http://google.com"
-    |> safe_concat(path)     # "http://google.com/test"
-    |> safe_concat(fragment) # "http://google.com/test#cats"
-    |> safe_concat(query)    # "http://google.com/test#cats?dogs_allowed=ño"
+    form_scheme(scheme)                   # "http://"
+    |> safe_concat(host)                  # "http://google.com"
+    |> safe_concat(path)                  # "http://google.com/test"
+    |> Kernel.<>(form_fragment(fragment)) # "http://google.com/test#cats"
+    |> Kernel.<>(form_query(query))       # "http://google.com/test#cats?dogs_allowed=ño"
   end
 
   defp form_scheme(""), do: ""
   defp form_scheme(scheme), do: scheme <> "://"
+
+  defp form_fragment(nil), do: ""
+  defp form_fragment(fragment), do: "#" <> fragment
+
+  defp form_query(nil), do: ""
+  defp form_query(query), do: "?" <> query
 
   defp safe_concat(left, right) do
     left  = left  || ""
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Norma.Mixfile do
   def project do
     [
       app: :norma,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
