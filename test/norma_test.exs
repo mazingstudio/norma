2ab2e985dcb30e59fb08ec299ad28a733c86980c
Zura Guerra <guerrazuramx@gmail.com>
2017-11-25 17:27:14 -0600

    Add more tests on scheme inference

--- test/norma_test.exs
+++ test/norma_test.exs
@@ -2,14 +2,21 @@ defmodule NormaTest do
   use ExUnit.Case
   @with_scheme "https://mazing.studio"
   @without_scheme "mazing.studio"
-  @without_scheme_but_port "//mazing.studio:21"
+  @with_scheme_but_path "mazing.studio/test"
+  @without_scheme_but_port "mazing.studio:21"
+  @without_scheme_but_port_alt "mazing.studio:1337"
+  @without_scheme_but_port_and_path "mazing.studio:1337/test"
   @with_path "https://mazing.studio/test"
   @with_fragment "https://mazing.studio#test"
   @with_www "https://www.mazing.studio"
   @full_example "//www.mazing.studio:1337/test#test"
 
-  test "scheme defaults to `http` when not provided (nor a port)",
-    do: assert Norma.normalize!(@without_scheme) == "http://mazing.studio"
+  test "scheme defaults to `http` when not provided (nor a port)" do
+    assert Norma.normalize!(@without_scheme) == "http://mazing.studio"
+    assert Norma.normalize!(@without_scheme_but_path) == "http://mazing.studio/test"
+    assert Norma.normalize!(@without_scheme_but_port_alt) == "http://mazing.studio:1337"
+    assert Norma.normalize!(@without_scheme_but_port_and_path) == "http://mazing.studio:1337/test"
+  end
 
   test "scheme gets infered from port",
     do: assert Norma.normalize!(@without_scheme_but_port) == "ftp://mazing.studio"
