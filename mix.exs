83bbcce2a93e5b9da7ec47f350a3134b67abe7b1
Zura Guerra <guerrazuramx@gmail.com>
2020-04-01 10:56:19 -0300

    Make minor updates to support files for the release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Norma.Mixfile do
   def project do
     [
       app: :norma,
-      version: "1.4.2",
+      version: "1.7.4",
       elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       description: description(),
